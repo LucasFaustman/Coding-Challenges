8c88a468b5bc50a0a6b16144ca4c3aa34c24861a
Lucas Faustman <lucasfaustman@gmail.com>
2022-02-17 13:22:09 -0800

    Added Feb 17 challenge

---
+++ 2022/February/17/main.js
@@ -0,0 +1,52 @@
+// You are going to be given an array of integers. Your job is to take that array and find an index N where the sum of the integers to the left of N is equal to the sum of the integers to the right of N. If there is no index that would make this happen, return -1.
+
+// For example:
+
+// Let's say you are given the array {1,2,3,4,3,2,1}:
+// Your function will return the index 3, because at the 3rd position of the array, the sum of left side of the index ({1,2,3}) and the sum of the right side of the index ({3,2,1}) both equal 6.
+
+// Let's look at another one.
+// You are given the array {1,100,50,-51,1,1}:
+// Your function will return the index 1, because at the 1st position of the array, the sum of left side of the index ({1}) and the sum of the right side of the index ({50,-51,1,1}) both equal 1.
+
+// Last one:
+// You are given the array {20,10,-80,10,10,15,35}
+// At index 0 the left side is {}
+// The right side is {10,-80,10,10,15,35}
+// They both are equal to 0 when added. (Empty arrays are equal to 0 in this problem)
+// Index 0 is the place where the left side and right side are equal.
+
+// Note: Please remember that in most programming/scripting languages the index of an array starts at 0.
+
+// Input:
+// An integer array of length 0 < arr < 1000. The numbers in the array can be any integer positive or negative.
+
+// Output:
+// The lowest index N where the side to the left of N is equal to the side to the right of N. If you do not find an index that fits these rules, then you will return -1.
+
+// Note:
+// If you are given an array with multiple answers, return the lowest correct index.
+
+function findEvenIndex(arr)
+{
+  
+//   I want to interate through the array one index at a time. if first index comes back false, goes to next index.
+//   Perhaps I can do a for loop. i will be the index.
+  let index = -1
+  for (let i = 0; i < arr.length; i++) {
+    let start = arr.slice(0,i+1).reduce((acc,num) => acc+num,0)
+    let end = arr.slice(i).reduce((acc,num) => acc+num,0)
+    
+    if (start === end) {
+      index = i
+    }
+  }
+  
+  return index
+  
+}
+
+
+// P Will always be an array of nums. Some numbers will be positive or negative
+// R: A single num which represents the index in which the sum of the left of N is equal to the next of the right of N
+// E [1,2,3,4,3,2,1] => 3, because index 3 is four, and to the left of four is 6 and right is 6
