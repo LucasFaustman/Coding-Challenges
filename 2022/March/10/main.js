ca29879dbdb9175441f3c122a6c73537704b7f9d
Lucas Faustman <lucasfaustman@gmail.com>
2022-03-10 11:10:20 -0800

    Added March 10 solution

---
+++ 2022/March/10/main.js
@@ -0,0 +1,16 @@
+// Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+// Examples:
+
+// solution('abc', 'bc') // returns true
+// solution('abc', 'd') // returns false
+
+function solution(str, ending){
+    // TODO: complete
+  //   Use the endswith method here!
+  return str.endsWith(ending)
+  }
+  
+  // P: two strings. always will be strings
+  // R: return true or false, depending of if str ends with ending
+  // E: 'prep', ep => true
