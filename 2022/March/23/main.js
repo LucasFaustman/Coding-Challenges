dd1860047e3d29e2136c63067c1fd179d6fb2cb8
Lucas Faustman <lucasfaustman@gmail.com>
2022-03-23 18:39:18 -0700

    Added March 23 coding challenge

---
+++ 2022/March/23/main.js
@@ -0,0 +1,17 @@
+// Messi goals function
+// Messi is a soccer player with goals in three leagues:
+
+// LaLiga
+// Copa del Rey
+// Champions
+// Complete the function to return his total number of goals in all three leagues.
+
+// Note: the input will always be valid.
+
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+    //   just add the 3 parameters with a return statement!
+      return laLigaGoals + copaDelReyGoals + championsLeagueGoals
+    }
+    // P: Parameters will all be numbers. All inputs are positive(valid) and whole numbers.
+    // R: Return a single value which is a number adding up all parameters
+    // E: 3,4,5 => 12
