f8b281fe70dabf85e7c1293dbe7adb0be5113d82
Lucas Faustman <lucasfaustman@gmail.com>
2022-04-05 21:48:19 -0700

    Added april 5 coding challenge

---
+++ 2022/April/5/main.js
@@ -0,0 +1,6 @@
+// Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+function removeExclamationMarks(s) {
+    // i want to isolate the exclamation marks , get rid of them, then join the string back together
+    return s.split('!').join('')
+     }
