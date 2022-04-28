c58321599bfc740ee56615c97ddfc087c69169bf
Lucas Faustman <lucasfaustman@gmail.com>
2022-04-28 11:56:09 -0700

    Added coding challenge for 04-28-22

--- 2022/April/28/main.js
+++ 2022/April/28/main.js
@@ -1,4 +1,4 @@
-// It's bonus time in the big city! The fatcats are rubbing their paws in anticipation... but who is going to make the most money?
+// It's bonus time in the big city! The fatcats are rubbing their paws in anticipation... but who is going to make the most money?-
 
 // Build a function that takes in two arguments (salary, bonus). Salary will be an integer, and bonus a boolean.
 
