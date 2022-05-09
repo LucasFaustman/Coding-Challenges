ea8868040aee769432846d250c74825fa085ea00
Lucas Faustman <lucasfaustman@gmail.com>
2022-05-08 20:16:51 -0700

    Added May 8 solution

---
+++ 2022/May/8/main.js
@@ -0,0 +1,16 @@
+// Create a function which answers the question "Are you playing banjo?".
+// If your name starts with the letter "R" or lower case "r", you are playing banjo!
+
+// The function takes a name as its only argument, and returns one of the following strings:
+
+// name + " plays banjo" 
+// name + " does not play banjo"
+// Names given are always valid strings.
+
+function areYouPlayingBanjo(name) {
+    //   checks the first character of a string
+    //   if first character starts with R or r, they play banjo
+      
+      return (name.charAt(0) === 'r' || name.charAt(0) === 'R') ? `${name} plays banjo` : `${name} does not play banjo`
+      ;
+    }
