b54d4b4ad8841f9fc3bb48172fda5c3a618da84e
Lucas Faustman <lucasfaustman@gmail.com>
2022-07-01 11:12:44 -0700

    Added July 1 challenge

---
+++ 2022/July/1/main.js
@@ -0,0 +1,30 @@
+// Kids drink toddy.
+// Teens drink coke.
+// Young adults drink beer.
+// Adults drink whisky.
+// Make a function that receive age, and return what they drink.
+
+// Rules:
+
+// Children under 14 old.
+// Teens under 18 old.
+// Young under 21 old.
+// Adults have 21 or more.
+
+function peopleWithAgeDrink(old) {
+    //   I can probably just utilize simple if else conditionals here
+      
+      if (old < 14) {
+        return 'drink toddy'
+      } else if (old < 18) {
+        return 'drink coke'
+      } else if (old < 21) {
+        return 'drink beer'
+      } else if (old >= 21) {
+        return 'drink whisky'
+      }
+    };
+    
+    // P: will be a number representing age
+    // R: return a string representing what the person will be drinking
+    // E: 22 => whisky
