cea83e9df41e8f47b0bddf0c46e1e2aa088e2893
Lucas Faustman <lucasfaustman@gmail.com>
2022-07-04 11:28:11 -0700

    Added July 4 challenge

---
+++ 2022/July/4/main.js
@@ -0,0 +1,20 @@
+// Return the Nth Even Number
+
+// Example(Input --> Output)
+
+// 1 --> 0 (the first even number is 0)
+// 3 --> 4 (the 3rd even number is 4 (0, 2, 4))
+// 100 --> 198
+// 1298734 --> 2597466
+// The input will not be 0.
+
+function nthEven(n){
+    // your code here
+  //   Im pretty sure I can utilize a simple return statement here
+  //   times number by 2 then minus 2
+    return n * 2 - 2
+  }
+  
+  // P: will be a number greater than 0. always will be an integer
+  // R: return a number representing the nth even number
+  // E: 3 => 4 (0,2,4)
