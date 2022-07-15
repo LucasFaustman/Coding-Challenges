345b14353596fd37eca187de76873b26a7c69f57
Lucas Faustman <lucasfaustman@gmail.com>
2022-07-15 11:50:46 -0700

    Added July 15 challenge

---
+++ 2022/July/15/maintwo.js
@@ -0,0 +1,13 @@
+// Find the total sum of internal angles (in degrees) in an n-sided simple polygon. N will be greater than 2.
+
+
+
+function angle(n) {
+    //   simple return statement. to find a sum of all angles, we can do n-2 * 180
+      
+      return (n-2) * 180
+    }
+    
+    // P: will be an integer greater than 2
+    // R" return an integer representing the total sum in degrees
+    // E 3 -> 180
