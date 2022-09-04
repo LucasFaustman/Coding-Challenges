ce6778f3f986b7e3674ec0291ef8cf7ee391ff2c
Lucas Faustman <lucasfaustman@gmail.com>
2022-09-04 11:49:23 -0700

    Added sept 4 coding challenge #3

---
+++ 2022/September/4/mainthree.js
@@ -0,0 +1,9 @@
+// Write a function that will compare two values, one will be a number and one will be a string. Return true if they are the same character (regardless of their different data types) and return false if they are not.
+
+function add(a, b){
+    //simple 2 equals sign comparison
+    return a == b
+  }
+  //P: will be 2 params, one a string and one a number. will always be a string and number
+  // R; return a boolean representing if the 2 params are equal in value, not type
+  // E: '1',1 => true
