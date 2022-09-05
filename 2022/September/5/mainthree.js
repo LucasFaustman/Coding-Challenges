68adc3e834ea46bcbd93986f757fe71de813eca7
Lucas Faustman <lucasfaustman@gmail.com>
2022-09-05 12:30:40 -0700

    Added Sept 5 coding challenge

---
+++ 2022/September/5/mainthree.js
@@ -0,0 +1,23 @@
+// Given 2 strings, a and b, return a string of the form: shorter+reverse(longer)+shorter.
+
+// In other words, the shortest string has to be put as prefix and as suffix of the reverse of the longest.
+
+// Strings a and b may be empty, but not null (In C# strings may also be null. Treat them as if they are empty.).
+// If a and b have the same length treat a as the longer producing b+reverse(a)+b
+
+function shorter_reverse_longer(a,b){
+    //I can use 3 conditionals here. if a.length > b.length, return string outlined in problem
+    
+    if (a.length > b.length) {
+      return `${b}${a.split('').reverse().join('')}${b}`
+    } else if (a.length === b.length) {
+      return `${b}${a.split('').reverse().join('')}${b}`
+    } else  {
+      return `${a}${b.split('').reverse().join('')}${a}`
+    }
+  }
+  
+  //P: will be two strings. might be empty but not null
+  // R: return a new string structures like this. shorter+reverse(longer)+shorter
+  //if same length, return b+reverse(a)+b
+  //E: 'first','abcde' => 'abcdetsrifabcde'
